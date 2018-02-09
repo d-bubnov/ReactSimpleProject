870d5f49acefb5a812af3c3aae13341ecfbb3c87
Denis Bubnov <denis.bubnov@arcadia.spb.ru>
2018-02-09 09:22:39 +0300

    Add video details (name, title) to video item

--- src/components/video/video_list_item.js
+++ src/components/video/video_list_item.js
@@ -3,7 +3,7 @@ import React from 'react';
 const VideoListItem = ({video}) => {
     // const video = props.video;
     console.log(video)
-    return <li>Video element</li>
+    return <li title={video.snippet.title}>{video.snippet.channelTitle}</li>
 }
 
 export default VideoListItem;
