b87710e5eb8b7ae58bae54eb6caa7fdc57db5b64
Denis Bubnov <denis.bubnov@arcadia.spb.ru>
2018-02-13 10:57:22 +0300

    Add new components, add new queries, add new handlers for GraphQL

--- src/index.js
+++ src/index.js
@@ -37,21 +37,9 @@ const client = new ApolloClient({
     link: createHttpLink({ uri: 'http://localhost:3000/graphql' })
 });
 
-const PhysicalPersons = ({ data: { loading, error, physicalPersons }}) => {
-    if (loading) {
-        console.log('loading');
-        return <p>Loading...</p>
-    } else if (error) {
-        console.log('error: ' + error);
-        return <p>{error.message}</p>
-    } else {
-        console.log('object: ' + physicalPersons);
-        return <ul>
-            { physicalPersons.map( pp => <li key={pp.id}>{pp.lastName}</li> ) }
-        </ul>
-    }
-}
 
+// ----------------------------------------------------------------------------------------------------------------
+// Get list of physical persons
 const physicalPersonsQuery = gql`
     query PhysicalPersonsQuery {
         physicalPersons {
@@ -62,7 +50,80 @@ const physicalPersonsQuery = gql`
     }
 `;
 
-const PhysicalPersonsWithData = graphql(physicalPersonsQuery)(PhysicalPersons);
+const PhysicalPersonsHandler = ({ data: { loading, error, physicalPersons }}) => {
+    if (loading) {
+        return <p>Loading...</p>
+    } else if (error) {
+        console.log('error: ' + error);
+        return <p>{error.message}</p>
+    } else {
+        console.log('physicalPersons: ' + JSON.stringify(physicalPersons));
+        return <ul>{ physicalPersons.map( pp => <li key={pp.id}>{pp.lastName} {pp.firstName}</li> ) }</ul>
+    }
+};
+
+const PhysicalPersonsComponent = graphql(physicalPersonsQuery)(PhysicalPersonsHandler);
+
+
+// ----------------------------------------------------------------------------------------------------------------
+// Get one physical person
+const physicalPersonQuery = gql`
+    query PhysicalPersonQuery($physicalPersonId: ID!) {
+        physicalPerson(id: $physicalPersonId) {
+            id
+            firstName
+            lastName
+            patronymic
+            phoneNumber
+        }
+    }
+`;
+
+const physicalPersonHandler = ({ data: { loading, error, physicalPerson }}) => {
+    if (loading) {
+        return <p>Loading person...</p>
+    } else if (error) {
+        console.log('error: ' + error);
+        return <p>An error occurred while loading data...</p>
+    } else {
+        console.log('physicalPerson: ' + JSON.stringify(physicalPerson));
+        return <p key={physicalPerson.id}>Name: {physicalPerson.firstName} {physicalPerson.lastName} {physicalPerson.patronymic}, phone: {physicalPerson.phoneNumber}</p>
+    }
+};
+
+const PhysicalPersonComponent = graphql(
+    physicalPersonQuery,
+    {
+        options: () => ({
+            variables: { physicalPersonId: '322845fc-0146-4e3b-80f9-3aafa5de584d' }
+        })
+    })(physicalPersonHandler);
+
+
+// -----------------------------------------------------------------------------------------------------------------
+// Get debts
+const debtsQuery = gql`
+    query DebtsQuery {
+        debts {
+            id
+            description
+        }
+    }
+`;
+
+const DebtsHandler = ({ data: { loading, error, debts }}) => {
+    if (loading) {
+        return <p>Loading...</p>
+    } else if (error) {
+        console.log('error: ' + error);
+        return <p>{error.message}</p>
+    } else {
+        console.log('debts: ' + JSON.stringify(debts));
+        return <ul>{ debts.map( debt => <li key={debt.id}>{debt.description}</li> ) }</ul>
+    }
+};
+
+const DebtsComponent = graphql(debtsQuery)(DebtsHandler);
 
 
 
@@ -70,7 +131,18 @@ const PhysicalPersonsWithData = graphql(physicalPersonsQuery)(PhysicalPersons);
 ReactDOM.render(
     <ApolloProvider client={client}>
         <div>
-            <PhysicalPersonsWithData />
+            <div>
+                <h4>Physical persons:</h4>
+                <PhysicalPersonsComponent/>
+            </div>
+            <div>
+                <h4>Main physical person:</h4>
+                <PhysicalPersonComponent/>
+            </div>
+            <div>
+                <h4>List of debts:</h4>
+                <DebtsComponent/>
+            </div>
         </div>
     </ApolloProvider>,
     document.getElementById("routing-block")
